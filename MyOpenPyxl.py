04e93f3616c464aa2e1ff0444df0b1a8a62048b6
pechjonka <pechjonka1986@gmail.com>
2023-10-20 20:34:16 +0100

    add requirements file

--- MyOpenPyxl.py
+++ MyOpenPyxl.py
@@ -1,24 +1,22 @@
 from openpyxl import Workbook, load_workbook
 
-# wb = Workbook()
-#
-# ws = wb.active
-#
-# ws1 = wb.create_sheet('Mysheet')
-#
-# ws2 = wb.create_sheet('Mysheet2', 0)
-#
-# ws.title = 'First Title'
-#
-# # ws2.title[1] = 'Second'
-#
-# ws['A5'] = 'ABC'
-#
-# ws['A4'] = 4
-#
-# d = ws.cell(row=4, column=2, value=10)
-#
-# wb.save(f'banana.xlsx')
+wb = Workbook()
+
+ws = wb.active
+
+ws1 = wb.create_sheet('Mysheet')
+
+ws2 = wb.create_sheet('Mysheet2', 0)
+
+ws.title = 'First Title'
+
+ws1['A5'] = 'ABC'
+
+ws2['A4'] = 4
+
+d = ws.cell(row=4, column=2, value=10)
+
+wb.save(f'banana.xlsx')
 
 wb = load_workbook('banana.xlsx')
 
